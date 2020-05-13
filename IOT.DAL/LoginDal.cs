46aa72ac9ac2a1624efd82d6244d36d19a81229b
74423 <74423@DESKTOP-VT335RP>
2020-05-13 13:48:03 +0800

    添加项目文件。

---
+++ IOT.DAL/LoginDal.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MODEL;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+
+namespace DAL
+{
+    public class LoginDal
+    {
+        string connStr= "Data Source=.;Initial Catalog=Lazy_e_commerce;Integrated Security=True";
+        //绑定角色下拉
+        public List<Roles> BandRoles()
+        {
+            using (var conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "select * from Roles";
+
+                DataTable dt = new DataTable();
+                var da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+
+                var rolesLists = new List<Roles>();
+                foreach (DataRow dr in dt.Rows)
+                {
+                    var rolesList = new Roles();
+                    rolesList.Rid = Convert.ToInt32(dr["Rid"]);
+                    rolesList.Rname = dr["Rname"].ToString();
+                    rolesList.Rstate = Convert.ToInt32(dr["Rstate"]);
+                    rolesLists.Add(rolesList);
+                }
+                return rolesLists;
+            }
+        }
+        //管理员列表显示
+        public List<LoginModel> LoginShow()
+        {
+            using (var conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "select  *  from Logins join Roles on Logins.Lrid=Roles.Rid";
+
+                DataTable dt = new DataTable();
+                var da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+
+                var loginLists = new List<LoginModel>();
+                foreach (DataRow dr in dt.Rows)
+                {
+                    var loginList = new LoginModel();
+                    loginList.Lid = Convert.ToInt32(dr["Lid"]);
+                    loginList.Lname = dr["Lname"].ToString();
+                    loginList.Lpass = dr["Lpass"].ToString();
+                    //loginList.Lportrait = dr["Lportrait"].ToString();
+                    loginList.Lrid = Convert.ToInt32(dr["Lrid"]);
+                    loginList.Rname = dr["Rname"].ToString();
+                    loginLists.Add(loginList);
+                }
+                return loginLists;
+            }
+        }
+        //添加管理员
+        public int AddLogin(LoginModel m)
+        {
+            using (var conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = string.Format("insert into Logins values('{0}','{1}','{2}')",m.Lname, Md6Encrypt(m.Lpass),m.Lrid);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+        //用户登录
+        public int UserLogin(UserModel m)
+        {
+            using (var conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = string.Format("select count(1) from UserLogin where Uname='{0}' and Upwd='{1}'",m.Uname, Md6Encrypt(m.Upwd));
+                return (int) cmd.ExecuteScalar();
+            }
+        }
+        //查询用户信息
+        public UserModel UserLogin(string name)
+        {
+            using (var conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = $"select * from UserLogin where Uname='{name}'";
+                DataTable dt = new DataTable();
+                var da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+
+                var loginLists = new List<UserModel>();
+                foreach (DataRow dr in dt.Rows)
+                {
+                    var loginList = new UserModel();
+                    loginList.Uid = Convert.ToInt32(dr["Uid"]);
+                    loginList.Uname = dr["Uname"].ToString();
+                    loginList.Upwd = dr["Upwd"].ToString();
+                    //loginList.Lportrait = dr["Lportrait"].ToString();
+                    loginList.Upwds = Convert.ToString(dr["Upwds"]);
+
+                    loginLists.Add(loginList);
+                }
+                return loginLists[0];
+            }
+        }
+        //注册
+        public int AddUser(UserModel m)
+        {
+            using (var conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = string.Format("insert into UserLogin values('{0}','{1}','{2}','{3}')", m.Uname, Md6Encrypt(m.Upwd),m.Upwds,m.UBalance);
+                return cmd.ExecuteNonQuery ();
+            }
+        }
+        //MD6加密
+        /// <summary>
+        /// MD6加密
+        /// </summary>
+        /// <param name="strSource"></param>
+        /// <returns></returns>
+        public static string Md6Encrypt(string strSource)
+        {
+            //把字符串放到byte数组中
+            byte[] bytIn = Encoding.Default.GetBytes(strSource);
+            //建立加密对象的密钥和偏移量
+            byte[] iv = { 102, 16, 93, 156, 78, 4, 218, 32 };//定义偏移量
+            byte[] key = { 55, 103, 246, 79, 36, 99, 167, 3 };//定义密钥
+                                                              //实例DES加密类
+            DESCryptoServiceProvider mobjCryptoService = new DESCryptoServiceProvider();
+            mobjCryptoService.Key = iv;
+            mobjCryptoService.IV = key;
+            ICryptoTransform encrypto = mobjCryptoService.CreateEncryptor();
+            //实例MemoryStream流加密密文件
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, encrypto, CryptoStreamMode.Write);
+            cs.Write(bytIn, 0, bytIn.Length);
+            cs.FlushFinalBlock();
+            string strOut = System.Convert.ToBase64String(ms.ToArray());
+            return strOut;
+
+        }
+    }
+}
---
+++ Webapi/Controllers/ShopController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.UI;
+using IOT.DAL;
+using IOT.Model;
+using SelectModel;
+
+namespace Webapi.Controllers
+{
+    public class ShopController : ApiController
+    {
+        ShopDAL dal = new ShopDAL();
+        // GET: api/Shop
+        public PageInfor Get(string Name = "", string Pricemin = "", string Pricemax = "", int Type = 0, int Band = 0, int CurrentPage = 1, int PageSize = 5)
+        {
+            var list = dal.Show().Where(s => s.Pstate = true);
+            if (!string.IsNullOrEmpty(Name))
+            {
+                list = list.Where(s => s.Pname.Contains(Name)).ToList();
+            }
+            if (!string.IsNullOrEmpty(Pricemin) && !string.IsNullOrEmpty(Pricemax))
+            {
+                list = list.Where(s => s.Pmarket >= Convert.ToInt32(Pricemin) && s.Pmarket <= Convert.ToInt32(Pricemax)).ToList();
+            }
+            if (Type != 0)
+            {
+                list = list.Where(s => s.Ptype == Type).ToList();
+            }
+            if (Band != 0)
+            {
+                list = list.Where(s => s.Ppinpai == Band).ToList(); ;
+            }
+            //实例化分页类
+            var p = new PageInfor();
+            //总记录数
+            p.TotalCount = list.Count();
+            //计算总页数
+            if (p.TotalCount == 0)
+            {
+                p.TotalPage = 1;
+            }
+            else if (p.TotalCount % PageSize == 0)
+            {
+                p.TotalPage = p.TotalCount / PageSize;
+            }
+            else
+            {
+                p.TotalPage = (p.TotalCount / PageSize) + 1;
+            }
+            //纠正当前页不正确的值
+            if (CurrentPage < 1)
+            {
+                CurrentPage = 1;
+            }
+            if (CurrentPage > p.TotalPage)
+            {
+                CurrentPage = p.TotalPage;
+            }
+            p.shopModels = list.Skip(CurrentPage * (CurrentPage - 1)).Take(PageSize).ToList();
+
+            p.CurrentPage = CurrentPage;
+            return p;
+        }
+
+
+        //商品详情
+        // GET: api/Shop/5
+        public ShopModel Getone(int did)
+        {
+            return dal.Fill(did);
+        }
+
+        // POST: api/Shop
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Shop/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Shop/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
